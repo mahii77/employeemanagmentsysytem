ea725c4d92e6b4f015663d1fef84d8113802fd52
mahin <mahinravi77@gmail.com>
2026-03-27 11:37:20 +0530

    Initial commit

---
+++ Employeeform.js
@@ -0,0 +1,13 @@
+const { use, useState } = require("react")
+
+const Employeeform = {{ selected Employee ,refresh}} => {
+    const [employee, setEmployee]=
+
+    useState({
+        name:"",
+        email:"",
+        department:"",
+        position:"",
+        salary:"",
+    });
+}
---
+++ employeelist.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EmployeeList = ({employees})=> {
+    return (
+    <table>
+        <thead>
+            <tr>
+                <th> Name </th>
+                <th> Email </th>
+                <th> Department </th>
+                <th> Postion</th>
+                <th> Salary</th>
+            </tr>
+
+        </thead>
+    </table>
+
+
+
+    )
+}
